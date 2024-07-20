4009bb5a62274040a8bdb2cebae4683be3dd1d79
AndriyV2504 <andreyvolyanik@gmail.com>
2024-07-20 14:07:48 +0300

    api params

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const fetchPhotos = async (query) => {
+const fetchPhotos = async (query, page = 1, perPage = 9) => {
   const API_KEY = "lfyP73mfHILzu2C-oFJmdLgJm9tc0LVu9oBaiCVj82o";
   const response = await axios.get("https://api.unsplash.com/search/photos", {
     headers: {
@@ -8,6 +8,8 @@ const fetchPhotos = async (query) => {
     },
     params: {
       query,
+      page,
+      perPage: perPage,
     },
   });
   return response.data;
