360a2e89332f7ddf256b06721d1511ca8180aaf7
AndriyV2504 <andreyvolyanik@gmail.com>
2024-07-20 18:27:24 +0300

    add loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,23 @@ import { useEffect, useState } from "react";
 import ImageGallery from "./ImageGallery/ImageGallery";
 import fetchPhotos from "../services/api";
 import SearchBar from "./SearchBar/SearchBar";
+import Loader from "./Loader/Loader";
 
 const App = () => {
   const [images, setImages] = useState([]);
   const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchImages = async () => {
       try {
+        setLoading(true);
         const response = await fetchPhotos(query);
-        setImages(response.images);
+        setImages(response.results);
       } catch (error) {
         console.log(error);
+      } finally {
+        setLoading(false);
       }
     };
     fetchImages;
@@ -22,6 +27,7 @@ const App = () => {
     <div>
       <SearchBar onSubmit={setQuery} />
       <ImageGallery images={images} />
+      {loading && <Loader />}
     </div>
   );
 };
