a36d3c0c46dc7700e152af527b7dd35ae4ffbe1e
AndriyV2504 <andreyvolyanik@gmail.com>
2024-07-21 15:26:01 +0300

    style LoadMore

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -1,7 +1,9 @@
+import styles from "./LoadMoreBtn.module.css";
+
 const LoadMoreBtn = ({ onClick }) => {
   return (
     <div>
-      <button type="button" onClick={onClick}>
+      <button type="button" onClick={onClick} className={styles.button}>
         Load more
       </button>
     </div>
