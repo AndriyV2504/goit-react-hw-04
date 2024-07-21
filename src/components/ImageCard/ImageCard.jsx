d3d2c9a576c433807cb8d1cc004a47ef2ffa0d15
AndriyV2504 <andreyvolyanik@gmail.com>
2024-07-21 15:04:28 +0300

    styles imageGallery

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -2,9 +2,9 @@ import styles from "./ImageCard.module.css";
 
 const ImageCard = ({ image, onClick }) => {
   return (
-    <div>
+    <div className={styles.card}>
       <img
-        className={styles.item}
+        className={styles.image}
         src={image.urls.small}
         alt={image.alt_description}
         onClick={() => onClick(image)}
